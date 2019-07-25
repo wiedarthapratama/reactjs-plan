323a45a64c763afd8b53c6d6bf18e76786acea44
wied <wied@wallezz.com>
2019-07-25 11:14:22 +0700

    config firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAV_aFv1bCdqmkcElfeJCTAk9F-iL0BwP0",
+    authDomain: "react-plan-7a349.firebaseapp.com",
+    databaseURL: "https://react-plan-7a349.firebaseio.com",
+    projectId: "react-plan-7a349",
+    storageBucket: "",
+    messagingSenderId: "100941551608",
+    appId: "1:100941551608:web:017d5b056650a01d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({timestampsInSnapshots: true});
+
+export default firebase;
